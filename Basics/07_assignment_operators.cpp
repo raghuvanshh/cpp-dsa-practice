c4b2e256a7c01f626997739b756380464b884659
raghuvanshh <rxvnsh@gmail.com>
2026-06-19 20:02:49 +0530

    A C++ program that demonstrates the use of assignment operators to modify a variable's value.
    Uses +=, -=, *=, and /= operators on a single variable and displays the updated value after each operation. Helps understand shorthand assignment operations in C++.

---
+++ Basics/07_assignment_operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num = 10;
+    num += 5; 
+    cout << num << endl;
+    num -= 3; 
+    cout << num << endl;
+    num *= 2; 
+    cout << num << endl;
+    num /= 4; 
+    cout << num << endl;
+    return 0;
+}
