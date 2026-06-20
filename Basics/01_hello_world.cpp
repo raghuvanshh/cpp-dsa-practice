fe0cecd472e9233b741587d1a903d7081116a5ff
raghuvanshh <rxvnsh@gmail.com>
2026-06-20 08:58:29 +0530

    .

--- Basics/01_hello_world.cpp
+++
@@ -1,7 +0,0 @@
-//hello world
-#include <iostream>
-using namespace std;
-int main(){
-    cout<<"Hello world";
-    return 0;
-}
--- Basics/02_input_output.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    string name;
-    cout<<"Enter your name: "; cin>>name;
-    cout<<"Your name is: "<<name;
-    return 0;
-}
