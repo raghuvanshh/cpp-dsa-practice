7933852b9c37a05bda5373a9f20eb0eda5ad377c
raghuvanshh <rxvnsh@gmail.com>
2026-06-20 09:12:12 +0530

    A C++ program that demonstrates basic arithmetic operations using two user-provided numbers.
    Takes two integers as input and performs multiplication, division, addition, and subtraction.

---
+++ 01_Basics/04_arithmetic_operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter value of a: "; cin>>a;
+    cout<<"Enter value of b: "; cin>>b;
+    cout<<"a x b = "<<a*b <<endl;
+    cout<<"a / b = "<<a/b <<endl;
+    cout<<"a + b = "<<a+b <<endl;
+    cout<<"b - a = "<<b-a <<endl;
+    return 0;
+}
